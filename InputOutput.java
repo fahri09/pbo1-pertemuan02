66d529c18b16620be7702661268e05dab406b8e8
fahri09 <fahrifahri09@gmail.com>
2020-04-21 15:31:15 +0800

    Buat Output

--- InputOutput.java
+++ InputOutput.java
@@ -2,11 +2,27 @@ import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
-  Scanner scanner = new Scanner(System.in);
-  String nama;
-  System.out.print("Masukkan nama anda: ");
-  nama = scanner.nextLine();
+ Scanner scanner = new Scanner(System.in);
+ String nama;
+ int jumlahSks;
+ double IPK;
 
+ System.out.println("=== PBO PERTEMUAN 02 ===");
+ System.out.println("==== Program Fahri =====");
+ System.out.print("Masukkan nama anda: ");
+ nama = scanner.nextLine();
+ System.out.println("Hai, "+nama); 
+
+ System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+ jumlahSks = scanner.nextInt();
+
+ System.out.print("Masukkan IPK Sementara :");
+ IPK = scanner.nextDouble();
+
+ double jumlah = IPK * jumlahSks;
+ System.out.println("Total Nilai = "+jumlah);
+ 
+ System.out.println("=== Berhasil!! ===");
 
  }
 }
